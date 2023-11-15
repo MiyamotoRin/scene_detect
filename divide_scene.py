edd6c4b6aff7a4d6931b9e2a9e9b903743a91c6f
MiyamotoRin <muhe.iroiro.thedoctor@digitalnature.slis.tsukuba.ac.jp>
2023-11-15 16:03:40 +0900

    test: 中断した際、途中から始めるときのコードを追加

--- divide_scene.py
+++ divide_scene.py
@@ -3,6 +3,8 @@ import os
 
 def divide_scene_from_all_videos(dir_path):
     files = os.listdir(dir_path)
+    print(files.index('134463208.mp4'))
+    files = files[files.index('134463208.mp4'):]
     for file_name in files:
         file_path = os.path.join(dir_path, file_name)
         if os.path.isfile(file_path) and file_path.lower().endswith(('.mp4', '.avi', '.mov')):
