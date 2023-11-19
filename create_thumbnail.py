13538eddc221313137d255e391a77d34291696e2
MiyamotoRin <muhe.iroiro.thedoctor@digitalnature.slis.tsukuba.ac.jp>
2023-11-19 19:38:10 +0900

    test: open clip の　コード追加

--- create_thumbnail.py
+++ create_thumbnail.py
@@ -3,24 +3,24 @@ import os
 
 # generate_thumbnail(input, output, options)
 
-def create_thumbnails_for_all_videos(directory_path, thumbnail_options):   
+def create_thumbnails_for_all_videos(input_path, output_path, thumbnail_options):   
     # ディレクトリ内の全てのファイルを取得
-    files = os.listdir(directory_path)
-
+    files = os.listdir(input_path)
     # ディレクトリ内の各ファイルに対して処理
     for file_name in files:
         # ファイルの完全なパスを生成
-        file_path = os.path.join(directory_path, file_name)
+        file_path = os.path.join(input_path, file_name)
         
         # ファイルが動画ファイルかどうかをチェック（例: .mp4）
         if os.path.isfile(file_path) and file_path.lower().endswith(('.mp4', '.avi', '.mov')):
             # サムネイルの保存先パスを生成
-            thumbnail_path = os.path.join('thumbnails', f'{os.path.splitext(file_name)[0]}.png')
+            thumbnail_path = os.path.join(output_path, f'{os.path.splitext(file_name)[0]}.png')
             print(thumbnail_path)
             # サムネイルを生成
             generate_thumbnail(file_path, thumbnail_path, thumbnail_options)
 
-input_path = 'scenes'
+input_path = "/Volumes/Steam_SSD/videos/scenes"
+output_path = "/Volumes/Steam_SSD/videos/thumbnails"
 
 options = {
 	'trim': False,
@@ -30,4 +30,4 @@ options = {
 	'type': 'thumbnail'
 }
 
-create_thumbnails_for_all_videos(input_path, options)
+create_thumbnails_for_all_videos(input_path, output_path, options)
--- divide_scene.py
+++ divide_scene.py
@@ -3,8 +3,8 @@ import os
 
 def divide_scene_from_all_videos(dir_path):
     files = os.listdir(dir_path)
-    print(files.index('134463208.mp4'))
-    files = files[files.index('134463208.mp4'):]
+    print(files.index('75289802.mp4'))
+    files = files[files.index('75289802.mp4'):]
     for file_name in files:
         file_path = os.path.join(dir_path, file_name)
         print(file_path)
