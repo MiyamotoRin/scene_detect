1005927610c4d3b9346c7c9f4a52231cd412ac86
MiyamotoRin <muhe.iroiro.thedoctor@digitalnature.slis.tsukuba.ac.jp>
2023-11-28 18:04:54 +0900

    test: usearchから取得できるようになる

--- search_video.py
+++ search_video.py
@@ -9,13 +9,13 @@ model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrai
 tokenizer = open_clip.get_tokenizer('ViT-B-32')
 
 # Define the directory path containing the images
-# directory_path = '/Volumes/Steam SSD/videos/thumbnails/'  # This path will need to be updated by the user
+directory_path = '/Volumes/Steam SSD/videos/thumbnails/'  # This path will need to be updated by the user
 
 # List all image files in the specified directory
 # image_files = [os.path.join(directory_path, file) for file in os.listdir(directory_path) if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
 
 # Define the texts to compare with
-texts = ["A man with a loaf of bread in his mouth is running."]
+texts = ["A man in the mirror."]
 text_tokens = tokenizer(texts)
 
 # Placeholder for the similarity scores and corresponding image paths
@@ -23,15 +23,15 @@ image_similarity = {text: [] for text in texts}
 
 # Load the Usearch index
 index = Index()
-index.load('index.usearch')
+# index.load('index.usearch')
+index = Index.restore('index.usearch', view=True)
 
 # Process each image and calculate similarity
 with torch.no_grad(), torch.cuda.amp.autocast():
     text_features = model.encode_text(text_tokens)
     text_features /= text_features.norm(dim=-1, keepdim=True)
     text_features = text_features.numpy()
-    print(text_features[0].shape)
-    matches: Matches = index.search(text_features[0], 5)
+    matches: Matches = index.search(text_features[0].flatten(), 5)
 
     #     # Update the image_similarity dictionary with the similarity scores
     #     for i, text in enumerate(texts):
