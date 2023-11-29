f12edd3a993d5d25a4bc9228075332f7ba0a78fb
MiyamotoRin <muhe.iroiro.thedoctor@digitalnature.slis.tsukuba.ac.jp>
2023-11-30 03:07:45 +0900

    fix: 関数の引数にDBのパスを追加する
    バックエンドサーバーを建てた際、パスを`app.py`などで入力できるようにするため

--- search_video.py
+++ search_video.py
@@ -6,7 +6,7 @@ from usearch.index import Index, Matches
 
 import sqlite3
 
-def get_video_index(texts):
+def get_video_index(texts, db_path):
     # Load the model and tokenizer from open_clip
     model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
     tokenizer = open_clip.get_tokenizer('ViT-B-32')
@@ -17,7 +17,8 @@ def get_video_index(texts):
     # Load the Usearch index
     index = Index()
     # index.load('index.usearch')
-    index = Index.restore('index.usearch', view=True)
+    index = Index.restore(db_path, view=True)
+    print(Index.metadata(db_path))
 
     # Process each image and calculate similarity
     with torch.no_grad(), torch.cuda.amp.autocast():
@@ -28,11 +29,11 @@ def get_video_index(texts):
     
     return matches[0].key
 
-def get_video_path(video_id):
+def get_video_path(video_id, db_path):
     video_id += 1
     
     # データベースに接続する
-    conn = sqlite3.connect('file_paths.db')
+    conn = sqlite3.connect(db_path)
 
     # カーソルオブジェクトを作成する
     cursor = conn.cursor()
@@ -49,6 +50,6 @@ def get_video_path(video_id):
     return record[1]
     
 if __name__ == '__main__':
-    video_id = get_video_index(["A fire works in the sky"])
-    path = get_video_path(video_id)
+    video_id = get_video_index(["A fire works in the sky"], 'index.usearch')
+    path = get_video_path(video_id, 'file_paths.db')
     print(path)
