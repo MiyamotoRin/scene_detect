58ce172235d49c28aff0007438216ea6c8c20230
MiyamotoRin <muhe.iroiro.thedoctor@digitalnature.slis.tsukuba.ac.jp>
2023-11-29 17:13:22 +0900

    test: シーンのテキストから、動画のパスを取得できるようになる
    - シーンのテキストを配列で渡している

--- search_video.py
+++ search_video.py
@@ -4,40 +4,59 @@ import torch
 import open_clip
 from usearch.index import Index, Matches
 
-# Load the model and tokenizer from open_clip
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
-tokenizer = open_clip.get_tokenizer('ViT-B-32')
+import sqlite3
 
-# Define the directory path containing the images
-directory_path = '/Volumes/Steam SSD/videos/thumbnails/'  # This path will need to be updated by the user
+def get_video_index(texts):
+    # Load the model and tokenizer from open_clip
+    model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+    tokenizer = open_clip.get_tokenizer('ViT-B-32')
 
-# List all image files in the specified directory
-# image_files = [os.path.join(directory_path, file) for file in os.listdir(directory_path) if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    # Define the directory path containing the images
+    directory_path = '/Volumes/Steam SSD/videos/thumbnails/'  # This path will need to be updated by the user
 
-# Define the texts to compare with
-texts = ["A man in the mirror."]
-text_tokens = tokenizer(texts)
+    # List all image files in the specified directory
+    # image_files = [os.path.join(directory_path, file) for file in os.listdir(directory_path) if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
 
-# Placeholder for the similarity scores and corresponding image paths
-image_similarity = {text: [] for text in texts}
+    # Define the texts to compare with
+    text_tokens = tokenizer(texts)
 
-# Load the Usearch index
-index = Index()
-# index.load('index.usearch')
-index = Index.restore('index.usearch', view=True)
+    # Placeholder for the similarity scores and corresponding image paths
+    image_similarity = {text: [] for text in texts}
 
-# Process each image and calculate similarity
-with torch.no_grad(), torch.cuda.amp.autocast():
-    text_features = model.encode_text(text_tokens)
-    text_features /= text_features.norm(dim=-1, keepdim=True)
-    text_features = text_features.numpy()
-    matches: Matches = index.search(text_features[0].flatten(), 5)
+    # Load the Usearch index
+    index = Index()
+    # index.load('index.usearch')
+    index = Index.restore('index.usearch', view=True)
 
-    #     # Update the image_similarity dictionary with the similarity scores
-    #     for i, text in enumerate(texts):
-    #         image_similarity[text].append((similarity_scores[0, i].item(), image_path))
+    # Process each image and calculate similarity
+    with torch.no_grad(), torch.cuda.amp.autocast():
+        text_features = model.encode_text(text_tokens)
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+        text_features = text_features.numpy()
+        matches: Matches = index.search(text_features[0].flatten(), 3)
+    
+    return matches[0].key
 
-# Sort the similarity scores for each text and get the top 5 images
-print(matches)
-print(len(matches))
-print(matches[0].key)
+def get_video_path(video_id):
+    video_id += 1
+    
+    # データベースに接続する
+    conn = sqlite3.connect('file_paths.db')
+
+    # カーソルオブジェクトを作成する
+    cursor = conn.cursor()
+
+    # SQLクエリを実行する
+    cursor.execute('SELECT * FROM file_paths WHERE id=?', (video_id,))
+
+    # 結果を取得する
+    record = cursor.fetchone()
+
+    # 接続を閉じる
+    conn.close()
+    
+    return record[1]
+    
+if __name__ == '__main__':
+    video_id = get_video_index(["A fire works in the sky"])
+    get_video_path(video_id)
